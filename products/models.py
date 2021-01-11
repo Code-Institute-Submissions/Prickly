9cd3869c4f6eb2376b6fa788693ab8bae4c77539
Liga <ligabaikova@gmail.com>
2021-01-11 17:59:07 +0000

    Refactor the models and its admin view, Change many_colors from a checkbox to a select dropdown

--- products/models.py
+++ products/models.py
@@ -11,23 +11,19 @@ class Category(models.Model):
 
     name = models.CharField(max_length=20)
 
-    def __str__(self):
-        return self.name
-
-
-class Color(models.Model):
-    name = models.CharField(max_length=20)
-    color_hex = ColorField(default='#FFFFFF')
-
+    # Change string representation of the object
     def __str__(self):
         return self.name
 
 
 # Creating a products table
 class Product(models.Model):
-    # overriding the products_product table name
-    class Meta:
-        db_table = 'products'
+    YES = 'Y'
+    NO = 'N'
+    MANY_COLORS = [
+        (YES, 'Yes'),
+        (NO, 'No'),
+    ]
 
     product_code = models.CharField(max_length=20, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -37,18 +33,19 @@ class Product(models.Model):
                                      decimal_places=1, default=0,
                                      null=True, blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    many_colors = models.BooleanField(default=False)
-    color = models.ForeignKey(Color, on_delete=models.SET_NULL,
-                              null=True, blank=True)
+    many_colors = models.CharField(max_length=1,
+                                   choices=MANY_COLORS,
+                                   help_text=('Will the product come in '
+                                              'multiple colors?'))
     main_pic = models.ImageField('thumbnail picture', null=True, blank=True)
-    pic2 = models.ImageField(null=True, blank=True)
-    pic3 = models.ImageField(null=True, blank=True)
-    pic4 = models.ImageField(null=True, blank=True)
+    pic2 = models.ImageField('additional picture 2', null=True, blank=True)
+    pic3 = models.ImageField('additional picture 3', null=True, blank=True)
+    pic4 = models.ImageField('additional picture 4', null=True, blank=True)
     added_date = models.DateTimeField(auto_now_add=True)
     release_date = models.DateTimeField('product release date',
-                                        help_text="""Select today/now as the input
-                                                  if the product is being
-                                                  published now.""")
+                                        help_text=('Select today/now as the '
+                                                   'inout if the product is '
+                                                   'being published now.'))
 
     def __str__(self):
         return self.name
@@ -56,3 +53,13 @@ class Product(models.Model):
     # Returns days/hours until the product release if release date is in future
     def release_countdown(self):
         pass
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=20)
+    color_hex = ColorField(default='#FFFFFF')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,
+                                null=True, blank=True)
+
+    def __str__(self):
+        return self.name
