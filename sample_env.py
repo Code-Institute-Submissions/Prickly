fb27cf4d5d49c0f298bc1fb4ba42c63ea89648de
Liga <ligabaikova@gmail.com>
2021-01-14 17:07:18 +0000

    Added sample_env.py file

---
+++ sample_env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault('STRIPE_PUBLIC_KEY', '')
+os.environ.setdefault('STRIPE_SECRET_KEY', '')
+os.environ.setdefault('STRIPE_WH_SECRET', '')
+os.environ.setdefault('SECRET_KEY', '')
+os.environ.setdefault('DEVELOPMENT', '1')
