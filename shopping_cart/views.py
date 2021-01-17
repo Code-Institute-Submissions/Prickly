d4669a1146a5c07951bed4deb9940b712d919e6b
Liga <ligabaikova@gmail.com>
2021-01-17 03:02:27 +0000

    feat: Add messages to shopping cart views

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -27,11 +27,11 @@ def add_item_to_cart(request, product_id):
     if product_id in list(cart.keys()):
         # If in the cart, increase quantity
         cart[product_id] += qty
-        messages.warning(request, f'Added more of "{product.name}".')
+        messages.success(request, f'Added more of "{product.name}".')
     else:
         # Add to the cart if not in it already
         cart[product_id] = qty
-        messages.warning(request, f'Added "{product.name}" to the cart.')
+        messages.success(request, f'Added "{product.name}" to the cart.')
 
     # assign values to cart
     request.session['cart'] = cart
@@ -44,15 +44,18 @@ def change_cart(request, product_id):
     Update the quantity if changed
     If quantity is 0, remove the item
     """
+    product = get_object_or_404(Product, pk=product_id)
     qty = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     # If quantity is bigger than 0, change it, otherwise remove item
     if qty > 0:
         cart[product_id] = qty
+        messages.success(request, f'Changed quantity of "{product.name}" '
+                                  'in the cart.')
     else:
         cart.pop(product_id)
-
+        messages.success(request, f'Removed "{product.name}" from the cart.')
     # assign values to cart
     request.session['cart'] = cart
     return redirect(reverse('cart'))
@@ -68,15 +71,17 @@ def remove_item_from_cart(request, product_id):
     """
 
     try:
+        product = get_object_or_404(Product, pk=product_id)
         cart = request.session.get('cart', {})
         # Remove item from the session cart
         cart.pop(product_id)
+        messages.success(request, f'Removed "{product.name}" from the cart.')
 
         # re-assign values to cart
         request.session['cart'] = cart
         # return successful status code
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'There was a problem removing your item: {e}')
         # return internal server error status code
         return HttpResponse(status=500)
-        print(e)
