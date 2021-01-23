8f5ad56d227dc74d8cdcd09d43e293ee8a261ffa
liigalized <37201376+liigalized@users.noreply.github.com>
2021-01-23 13:45:53 +0000

    feat: Update memebrship template
    Use memebrship model to loop through all memebrships and display them

--- memberships/views.py
+++ memberships/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Membership
 
 
 def memberships(request):
-    """ A view to return the memberships page """
-    return render(request, 'memberships/memberships.html')
+    """
+    A view to return the memberships page
+    """
+
+    # Get all membership model entries
+    memberships = Membership.objects.all()
+    template = 'memberships/memberships.html'
+    context = {
+        'memberships': memberships,
+    }
+    return render(request, template, context)
