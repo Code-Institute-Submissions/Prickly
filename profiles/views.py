19178ea2a9b422470531a603f1d1b187192f92cc
Liga Baikova <lb@MacBook-Air.fritz.box>
2021-01-24 00:18:11 +0000

    feat: Allow user to update their profile details

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from memberships.models import Membership
 from .models import Profile
 from .forms import ProfileForm
@@ -8,13 +9,26 @@ def profile(request):
     """ A view to return the profile page """
     profile = get_object_or_404(Profile, user=request.user)
     membership = get_object_or_404(Membership, name=profile.membership)
-    profile_form = ProfileForm(instance=profile)
+
+    if request.method == 'POST':
+        profile_form = ProfileForm(request.POST, instance=profile)
+        if profile_form.is_valid():
+            profile_form.save()
+            messages.success(request, 'Well done you! Your '
+                                      'details were updated :)')
+        else:
+            messages.error(request, 'Uh oh, something went wrong. '
+                                    'Try again and make sure all '
+                                    'fields are valid!')
+    else:
+        profile_form = ProfileForm(instance=profile)
 
     template = 'profiles/profile.html'
     context = {
         'profile': profile,
         'membership': membership,
         'profile_form': profile_form,
+        'on_profile': True,
     }
 
     return render(request, template, context)
