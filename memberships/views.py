b8ce7faf570c4552db99f9b60c247f53710d4b6d
Liga Baikova <lb@Ligas-Air.fritz.box>
2021-01-24 10:55:26 +0000

    feat: Store selected membership type in a session variable

--- memberships/views.py
+++ memberships/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Membership
 
 
@@ -14,3 +14,15 @@ def memberships(request):
         'memberships': memberships,
     }
     return render(request, template, context)
+
+
+def membership_type(request):
+    """
+    Capture membership type selected by user, store it in
+    session variable and redirect user to the signup page
+    """
+
+    membership_type = request.POST.get('membership_type')
+    request.session['membership'] = membership_type
+
+    return redirect(reverse('account_signup'))
