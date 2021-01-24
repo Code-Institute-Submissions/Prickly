3320cba7f58f28624ae11bc8a212cb5e8920d907
Liga Baikova <liga.baikova@gmail.com>
2021-01-24 12:55:46 +0000

    fix: Add membership_type view to urls.py

--- memberships/urls.py
+++ memberships/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.memberships, name="memberships"),
+    path('membership_type/', views.membership_type,
+         name="membership_type"),
     path('membership_checkout/', views.membership_checkout,
          name="membership_checkout"),
 ]
