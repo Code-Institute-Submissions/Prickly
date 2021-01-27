ac0d34b3ff9826be77dd1db2ed7ba5bcac242835
Liga Baikova <liga.baikova@gmail.com>
2021-01-27 00:14:16 +0000

    feat: Update admin view for review model

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Review
 
 
-admin.site.register(Review)
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'product',
+        'user',
+        'rating',
+    )
