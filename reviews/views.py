8ccec982c65beb59cbf92c751449778983b569d2
Liga Baikova <liga.baikova@gmail.com>
2021-01-27 20:39:06 +0000

    feat: Allow only one review per user per item

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 
+from .models import Review
 from .forms import ReviewForm
 from products.models import Product
 from profiles.models import Profile
@@ -25,17 +26,29 @@ def add_review(request, product_id):
     # Add details to the form
     review_form = ReviewForm(review_details)
 
-    # If form is valid, add user and product and save
-    if review_form.is_valid():
-        review = review_form.save(commit=False)
-        review.user = user
-        review.product = product
-        review.save()
-        # Success message if added
-        messages.success(request, 'Thanks! Your review was added :)')
+    # Check if the user has already reviewed the item
+    existing_review = Review.objects.filter(user=user, product=product)
+    # If count of reviews is > 0, return an erro
+    if existing_review.count() > 0:
+        messages.error(request, 'You have already reviewed this item!')
+
+    # If no reviews are found, add the review
     else:
-        # Error message if form was invalid
-        messages.error(request, 'Uh oh, something went wrong. '
-                                'Make sure the form is valid.')
+        # If form is valid, add user and product and save
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.user = user
+            review.product = product
+            review.save()
+            # Success message if added
+            messages.success(request, 'Thanks! Your review was added :)')
+        else:
+            # Error message if form was invalid
+            messages.error(request, 'Uh oh, something went wrong. '
+                                    'Make sure the form is valid.')
 
     return redirect(reverse('product_item', args=(product_id,)))
+
+
+def edit_review(request, review_id):
+    pass
