dff6cd8cc33a56e486de8dc3610cac31e3e0395e
Liga Baikova <liga.baikova@gmail.com>
2021-01-30 01:40:45 +0000

    feat: Add sending emails functionality

--- prickly/settings.py
+++ prickly/settings.py
@@ -121,8 +121,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -255,3 +253,17 @@ STRIPE_PRICE_ID_BASIC = os.getenv('STRIPE_PRICE_ID_BASIC', '')
 STRIPE_PRICE_ID_SUPREME = os.getenv('STRIPE_PRICE_ID_SUPREME', '')
 STRIPE_PRICE_ID_ULTIMATE = os.getenv('STRIPE_PRICE_ID_ULTIMATE', '')
 DOMAIN_URL = os.getenv('DOMAIN_URL', '')
+
+# Display e-mails in terminal during development
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'prickly@example.com'
+# Send e-mails to user's e-mail address in deployment
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
