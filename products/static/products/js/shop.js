f1a8da1b790648398b068564fdb1588ceeb1a303
Liga Baikova <liga.baikova@gmail.com>
2021-01-30 00:48:23 +0000

    fix: Update image links for shop nav

--- products/static/products/js/shop.js
+++ products/static/products/js/shop.js
@@ -1,7 +1,7 @@
 const shopButton = document.querySelectorAll('.shop-button')
 shopButton.forEach(btn => {
     const categoryName = btn.innerHTML.toLowerCase();
-    btn.style.backgroundImage = `url('/media/${categoryName}-btn.png')`;
+    btn.style.backgroundImage = `url(/media/${categoryName}-btn.png)`;
     console.log(categoryName)
 });
 
